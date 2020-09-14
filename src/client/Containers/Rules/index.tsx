8a16b88972d40f15ec48eee5e3212e43a8333252
Chace Stewart <chacestew@gmail.com>
2020-09-14 16:07:26 +0200

    beginnings of a real rule page

--- src/client/Containers/Rules/index.tsx
+++ src/client/Containers/Rules/index.tsx
@@ -1,18 +1,84 @@
 import React from 'react';
+import pic from '../../../1.png';
+import palette from '../../utils/palette';
 
 export default () => (
   <div>
-    <p>
-      <span>To win</span> simply win three boards of tic-tac-toe in a row to win the game.
-    </p>
-    <p>
-      <span>But</span> The board you can play is decided by the your opponent's last move.
-    </p>
-    <p>
-      <span>Except</span>
-      If you start the game, or would be forced to play in a full board, you can choose
-      any open board.
-    </p>
-    <p>That's it - have fun!</p>
+    <div
+      css={`
+        display: flex;
+        padding: 1em;
+      `}
+    >
+      <div
+        css={`
+          width: 55%;
+          color: ${palette.textColor2};
+          padding-right: 1em;
+        `}
+      >
+        Ultimate tic-tac-toe is played over nine smaller tic-tac-toe boards. You must win
+        three smaller boards in a row to win the game.
+      </div>
+      <img
+        css={`
+          background-color: ${palette.gameBarBg};
+          // box-shadow: 0px 2px 2px rgba(204, 197, 185, 0.5);
+          width: 45%;
+        `}
+        src={pic}
+      />
+    </div>
+    <div
+      css={`
+        display: flex;
+        padding: 1em;
+      `}
+    >
+      <img
+        css={`
+          background-color: ${palette.gameBarBg};
+          // box-shadow: 0px 2px 2px rgba(204, 197, 185, 0.5);
+          width: 45%;
+        `}
+        src={pic}
+      />
+      <div
+        css={`
+          width: 55%;
+          color: ${palette.textColor2};
+          padding-left: 1em;
+        `}
+      >
+        However, plan carefully as players will choose where their opponent will play .
+        For example, here X plays in the top-right cell, meaning O play their next turn in
+        the top-right board.
+      </div>
+    </div>
+    <div
+      css={`
+        display: flex;
+        padding: 1em;
+      `}
+    >
+      <div
+        css={`
+          width: 55%;
+          color: ${palette.textColor2};
+          padding-right: 1em;
+        `}
+      >
+        A board that is won or fill is not playable. If you are sent there, you may choose
+        any open board to play in.
+      </div>
+      <img
+        css={`
+          background-color: ${palette.gameBarBg};
+          // box-shadow: 0px 2px 2px rgba(204, 197, 185, 0.5);
+          width: 45%;
+        `}
+        src={pic}
+      />
+    </div>
   </div>
 );
