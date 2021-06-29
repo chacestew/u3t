9e2d3855d68fac7ab2aa909c77f7d904a1abc350
Chace Stewart <chacestew@gmail.com>
2021-06-29 16:40:32 +0200

    Try out code split routes

--- client/src/Containers/App.tsx
+++ client/src/Containers/App.tsx
@@ -1,5 +1,5 @@
 import { ClientSocket } from '@u3t/common';
-import React, { useEffect, useState } from 'react';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { io } from 'socket.io-client';
 import styled from 'styled-components';
@@ -8,15 +8,15 @@ import { useTracking } from '../hooks/useTracking';
 import GlobalStyle from '../styles/global';
 import { media } from '../styles/mixins';
 import { gridSize } from '../utils/palette';
-import About from './About/About';
-import Contact from './Contact/contact';
+// import About from './About/About';
+// import Contact from './Contact/contact';
 import Footer from './Footer';
 import Header from './Header';
-import Home from './Home';
-import HotSeat from './HotSeat';
-import Play from './Lobby';
-import PlayAI from './PlayAI';
-import Rules from './Rules';
+// import Home from './Home';
+// import HotSeat from './HotSeat';
+// import Play from './Lobby';
+// import PlayAI from './PlayAI';
+// import Rules from './Rules';
 
 const Main = styled.main`
   display: flex;
@@ -38,6 +38,14 @@ const socketURL =
 
 const socket: ClientSocket = io(socketURL, { path: '/ws' });
 
+const Home = lazy(() => import('./Home'));
+const Lobby = lazy(() => import('./Lobby'));
+const About = lazy(() => import('./About/About'));
+const Contact = lazy(() => import('./Contact/contact'));
+const HotSeat = lazy(() => import('./HotSeat'));
+const PlayAI = lazy(() => import('./HotSeat'));
+const Rules = lazy(() => import('./HotSeat'));
+
 function App() {
   useTracking();
   const [deferredInstallPrompt, setDeferredInstallPrompt] =
@@ -56,34 +64,36 @@ function App() {
       <GlobalStyle />
       <Header />
       <Main>
-        <Switch>
-          <Route exact path="/">
-            <Home socket={socket} />
-          </Route>
-          <Route
-            exact
-            path="/game/:lobbyId"
-            render={(routeProps: any) => <Play socket={socket} {...routeProps} />}
-          />
-          <Route
-            exact
-            path="/game/:lobbyId/spectate"
-            render={(routeProps: any) => (
-              <Play socket={socket} spectator {...routeProps} />
-            )}
-          />
-          <Route path="/local" component={HotSeat} />
-          <Route path="/ai" component={PlayAI} />
-          <Route path="/rules" component={Rules} />
-          <Route
-            path="/about"
-            render={(routeProps) => (
-              <About {...routeProps} deferredInstallPrompt={deferredInstallPrompt} />
-            )}
-          />
-          <Route path="/contact" component={Contact} />
-          <Redirect to="/" />
-        </Switch>
+        <Suspense fallback={null}>
+          <Switch>
+            <Route exact path="/">
+              <Home socket={socket} />
+            </Route>
+            <Route
+              exact
+              path="/game/:lobbyId"
+              render={(routeProps: any) => <Lobby socket={socket} {...routeProps} />}
+            />
+            <Route
+              exact
+              path="/game/:lobbyId/spectate"
+              render={(routeProps: any) => (
+                <Lobby socket={socket} spectator {...routeProps} />
+              )}
+            />
+            <Route path="/local" component={HotSeat} />
+            <Route path="/ai" component={PlayAI} />
+            <Route path="/rules" component={Rules} />
+            <Route
+              path="/about"
+              render={(routeProps) => (
+                <About {...routeProps} deferredInstallPrompt={deferredInstallPrompt} />
+              )}
+            />
+            <Route path="/contact" component={Contact} />
+            <Redirect to="/" />
+          </Switch>
+        </Suspense>
       </Main>
       <Footer deferredInstallPrompt={deferredInstallPrompt} />
     </>
