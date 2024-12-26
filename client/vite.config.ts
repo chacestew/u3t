23f9c22756a4f35e5e15c7861bc88b0ef1e3f056
Chace <chacestew@gmail.com>
2024-12-26 16:46:49 +0000

    Vite (#6)
    * Add vite
    
    * Remove webpack and old dependencies
    
    * eslint
    
    * lint and format
    
    * typescript bumps
    
    * Replace custom service worker with Vite PWA plugin
    
    * lock file
    
    * Fix lock files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { createHtmlPlugin } from 'vite-plugin-html'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,23 @@ export default defineConfig({
     react(),
     createHtmlPlugin({
       entry: '../src/index.tsx',
-      template: './public/index.html'})
+      template: './public/index.html',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: false,
+      manifestFilename: 'manifest.json',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'favicon-16x16.png',
+        'favicon-32x32.png',
+        'browserconfig.xml',
+        'mstile-150x150.png',
+        'safari-pinned-tab.svg',
+      ],
+      strategies: 'generateSW',
+    }),
   ],
-  
-})
+});
